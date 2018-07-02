c300a88b6f98291a6a7a1a6c5d3ba3af39bc1c8e
Patrick Ng <patricng@amazon.com>
2018-07-02 15:03:24 +0000

    new params

--- run_infer_sent.sh
+++ run_infer_sent.sh
@@ -5,15 +5,18 @@ set -xu
 group_timestamp=$(date -u +\%Y\%m\%d-\%H\%M\%S)
 run_idx=0
 
-for optimizer in " " "--optimizer=adam"; do
+# for optimizer in "--optimizer=adam"; do
+for optimizer in ""; do
     for encoder_type in ConvNetEncoder BLSTMEncoder ; do
         ((run_idx+=1))
         idx=$(printf "%02d" $run_idx)
+        rnd=$(pwgen 5 1)
 
         unbuffer python ./train_nli.py \
             $optimizer \
             --encoder_type=$encoder_type \
             --gpu_id=0 \
-        &> >(tee stderr tmp/train-${group_timestamp}-${idx}-$(pwgen 5 1).log)
+            --outputdir=tmp/train-${group_timestamp}-${idx}-${rnd}/
+        &> >(tee stderr tmp/train-${group_timestamp}-${idx}-${rnd}.log)
     done
 done
